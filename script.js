60e276afb6e26f7284c5247a439118aeb580e417
Srabasti Choudhury <89031885+srabasti20@users.noreply.github.com>
2023-12-05 20:00:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const inputbox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-container");
+function addtask(){
+    if(inputbox.value ===''){
+        alert("You must write something");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listcontainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="&#128465;";
+        span.innerHTML.color="blue";
+        li.appendChild(span);
+    }
+    inputbox.value="";
+    saveData();
+
+}
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+
+    }
+},false);
+function saveData(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+
+}
+function showlist(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+
+}
+showlist();
